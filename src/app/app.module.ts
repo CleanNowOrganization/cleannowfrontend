2c03e3af5709698ea734639ded7a0c3c91b1873c
JackBright1510 <gsalyrosasc1@upao.edu.pe>
2023-06-28 01:31:01 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,30 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './theme/shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './theme/layout/admin/admin.component';
-import { AuthComponent } from './theme/layout/auth/auth.component';
-import { NavigationComponent } from './theme/layout/admin/navigation/navigation.component';
-import { NavContentComponent } from './theme/layout/admin/navigation/nav-content/nav-content.component';
-import { NavGroupComponent } from './theme/layout/admin/navigation/nav-content/nav-group/nav-group.component';
-import { NavCollapseComponent } from './theme/layout/admin/navigation/nav-content/nav-collapse/nav-collapse.component';
-import { NavItemComponent } from './theme/layout/admin/navigation/nav-content/nav-item/nav-item.component';
+import { ConfigurationComponent } from './theme/layout/admin/configuration/configuration.component';
 import { NavBarComponent } from './theme/layout/admin/nav-bar/nav-bar.component';
 import { NavLeftComponent } from './theme/layout/admin/nav-bar/nav-left/nav-left.component';
 import { NavSearchComponent } from './theme/layout/admin/nav-bar/nav-left/nav-search/nav-search.component';
 import { NavRightComponent } from './theme/layout/admin/nav-bar/nav-right/nav-right.component';
-import { ConfigurationComponent } from './theme/layout/admin/configuration/configuration.component';
+import { NavCollapseComponent } from './theme/layout/admin/navigation/nav-content/nav-collapse/nav-collapse.component';
+import { NavContentComponent } from './theme/layout/admin/navigation/nav-content/nav-content.component';
+import { NavGroupComponent } from './theme/layout/admin/navigation/nav-content/nav-group/nav-group.component';
+import { NavItemComponent } from './theme/layout/admin/navigation/nav-content/nav-item/nav-item.component';
+import { NavigationComponent } from './theme/layout/admin/navigation/navigation.component';
+import { AuthComponent } from './theme/layout/auth/auth.component';
 
 import { ToggleFullScreenDirective } from './theme/shared/full-screen/toggle-full-screen';
 
 /* Menu Items */
+import { NgbButtonsModule, NgbDropdownModule, NgbNavModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 import { NavigationItem } from './theme/layout/admin/navigation/navigation';
-import { NgbButtonsModule, NgbDropdownModule, NgbTooltipModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [
@@ -40,7 +41,8 @@ import { NgbButtonsModule, NgbDropdownModule, NgbTooltipModule, NgbNavModule } f
         NavSearchComponent,
         NavRightComponent,
         ConfigurationComponent,
-        ToggleFullScreenDirective
+        ToggleFullScreenDirective,
+        FooterComponent
     ],
     imports: [
         BrowserModule,
