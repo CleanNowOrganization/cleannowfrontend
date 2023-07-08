771380aef06fdb441fb289b2b42c8e4cf72be17b
JackBright1510 <gsalyrosasc1@upao.edu.pe>
2023-07-08 17:32:57 -0500

    Create pagos.module.ts

---
+++ src/app/cleannow/pages/pagos/pagos.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PagosComponent } from './pagos.component';
+
+@NgModule({
+  declarations: [PagosComponent],
+  imports: [CommonModule],
+  exports: [PagosComponent]
+})
+export class PagosModule { }
