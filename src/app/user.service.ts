984298d69119237793a421f5e05b50a4cb1827a9
Toshiro Escobedo Correa <aescobedoc4@upao.edu.pe>
2023-07-15 15:28:16 -0500

    signg auth

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,19 +2,22 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class UserService {
-  private userData = new BehaviorSubject<any>(null);
-
-  constructor() { }
+  private userData = new BehaviorSubject<any>(this.getLocalUserData());
 
   setUserData(data: any) {
+    localStorage.setItem('userData', JSON.stringify(data));
     this.userData.next(data);
   }
 
   getUserData() {
     return this.userData.asObservable();
+  }
+
+  private getLocalUserData() {
+    const userData = localStorage.getItem('userData');
+    return userData ? JSON.parse(userData) : null;
   }
 }
